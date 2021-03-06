fe31734a009fcca25632740859cc12f738901d2a
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-05 22:45:11 -0300

    Entrega da atividade NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string nome;
+            string sobrenome;
+            string nomeCompleto;
+            string nomeCatalogo;
+            
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Veja seu nome de diferentes formas");
+            Console.WriteLine("----------------------------------");
+            Console.ResetColor();
+
+            Console.Write("Digite seu primeiro nome:");
+            nome = Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome:");
+            sobrenome = Console.ReadLine();
+
+            nomeCompleto = $"{nome} {sobrenome}";
+            nomeCatalogo = $"{sobrenome.ToUpper()}, {nome}.";
+
+            Console.WriteLine();
+            Console.WriteLine($"Nome Completo: {nomeCompleto}");
+            Console.WriteLine($"Nome Catalogo: {nomeCatalogo}");
+
+        }
+    }
+}
